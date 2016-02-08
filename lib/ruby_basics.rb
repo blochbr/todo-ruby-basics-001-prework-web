f118caae447e08be9e59e60b397c2048c11e3adf
blochbr <blochbr@gmail.com>
2016-02-08 19:52:10 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
-def division(num1, num2)
- 
+def division(num1 = 36, num2 = 6)
+  answer = num1 / num2
+  return answer
 end
 
-def assign_variable(value)
-  
+def assign_variable(value = "Bob")
+  name = value
+  return name
 end
 
-def argue
-  
+def argue(myArgument = "I'm right and you are wrong")
+  return myArgument
 end
 
-def greeting
-
+def greeting(salut, name)
+  return salut + name
 end
 
-def return_a_value
-
+def return_a_value(nice = "Nice")
+  return nice
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(level = "expert")
+  return level
 end
 
-def pizza_party
-
+def pizza_party(cheese = "cheese")
+  return cheese
 end
